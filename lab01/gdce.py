5cd7a3d806150c9215c5ee5246aa571128b4d56e
Jamie K <jkerney@berkeley.edu>
2024-09-15 16:47:08 -0700

    trivial dead code elimination

---
+++ lab01/gdce.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+def is_pure(instr):
+    if instr["op"] in ["add", "mul", "sub", "div", "eq", "lt", "gt", "le", "ge", "not", "and", "or"]:
+        return True
+    return False
+
+def optimize(function):
+    used = []
+    for instr in function["instrs"]:
+        if "args" in instr.keys():
+            used += instr["args"]
+
+    for idx in range(len(function["instrs"])):
+        instr = function["instrs"][idx]
+        if not "dest" in instr.keys():
+            continue
+
+        if (not instr["dest"] in used) and is_pure(instr):
+            function["instrs"][idx] = None
+
+    function["instrs"] = [instr for instr in function["instrs"] if instr not None]
+
+
+if __name__ == "__main__":
+    prog = json.load(sys.stdin)
+    functions = prog["functions"]
+    prog["functions"] = [optimize(f) for f in functions]
+    json.dump(prog, sys.stdout, indent=2)
