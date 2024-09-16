f008c425351011b5c0d5471771605bb8d82390b3
Jamie K <jkerney@berkeley.edu>
2024-09-16 14:23:01 -0700

    fixed gdce

--- lab01/gdce.py
+++ lab01/gdce.py
@@ -2,33 +2,36 @@ import json
 import sys
 
 def is_pure(instr):
-    if instr["op"] in ["add", "mul", "sub", "div", "eq", "lt", "gt", "le", "ge", "not", "and", "or"]:
+    if instr["op"] in ["add", "mul", "sub", "div", "eq", "lt", "gt", "le", "ge", "not", "and", "or", "const"]:
         return True
     return False
 
-def optimize(function):
+def gdce(function):
     con = True
     while con:
         used = []
         con = False
-	    for instr in function["instrs"]:
-	        if "args" in instr.keys():
-	            used += instr["args"]
-	
-	    for idx in range(len(function["instrs"])):
-	        instr = function["instrs"][idx]
-	        if not "dest" in instr.keys():
-	            continue
-	
-	        if (not instr["dest"] in used) and is_pure(instr):
-	            function["instrs"][idx] = None
+        for instr in function["instrs"]:
+            if instr is None:
+                continue
+            if "args" in instr.keys():
+                used += instr["args"]
+
+        for idx in range(len(function["instrs"])):
+            instr = function["instrs"][idx]
+            if instr is None:
+                continue
+            if not "dest" in instr.keys():
+                continue
+            if (not instr["dest"] in used) and is_pure(instr):
+                function["instrs"][idx] = None
                 con = True
 
-    function["instrs"] = [instr for instr in function["instrs"] if instr not None]
-
+    function["instrs"] = [instr for instr in function["instrs"] if not instr is None]
+    return function
 
 if __name__ == "__main__":
     prog = json.load(sys.stdin)
     functions = prog["functions"]
-    prog["functions"] = [optimize(f) for f in functions]
+    prog["functions"] = [gdce(f) for f in functions]
     json.dump(prog, sys.stdout, indent=2)
