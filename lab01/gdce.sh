14951be1b5a4aec67c8040d5d7bdcbfc04a397f4
Jamie K <jkerney@berkeley.edu>
2024-09-20 11:13:21 -0700

    removed lab01

--- lab01/gdce.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in ../../bril/benchmarks/mem/*.bril; do
-    # echo "Processing $file"
-    # Run the command for each file
-    pre_lc=$(cat "$file" | wc -l)
-    opt_lc=$(cat "$file" | bril2json | python3 gdce.py | bril2txt | wc -l)
-    echo "$file,$pre_lc,$opt_lc"
-done
