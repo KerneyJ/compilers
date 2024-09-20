6c579ec084d69358da313b8e4e3994d62a6e0f45
Jamie K <jkerney@berkeley.edu>
2024-09-20 10:57:22 -0700

    moved directories

---
+++ task01/ldce.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for file in ../../bril/benchmarks/mem/*.bril; do
+    # echo "Processing $file"
+    # Run the command for each file
+    pre_lc=$(cat "$file" | wc -l)
+    opt_lc=$(cat "$file" | bril2json | python3 ldce.py | bril2txt | wc -l)
+    echo "$file,$pre_lc,$opt_lc"
+done
