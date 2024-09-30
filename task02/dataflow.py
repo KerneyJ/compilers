ee3200a94749c43f5bbde214eea486b6cd54ce63
Jamie K <jkerney@berkeley.edu>
2024-09-29 17:12:38 -0700

    wip

--- task02/dataflow.py
+++ task02/dataflow.py
@@ -1,24 +1,33 @@
 import json
 import sys
 
-class bb: 
+class bb:
     TERM = ['br', 'jmp', 'ret']
+
     def __init__(self, instrs, name, num, func_name):
         self.instrs = instrs
         self.name = name
         self.parents = []
+        self.kids = []
+        self.parent_state = []
         self.term = self.instrs[-1]
         self.num = num
         self.func_name = func_name
 
     def __str__(self):
-        return "Name: " + self.name + " Parents: " + ", ".join([p.name for p in self.parents])
+        s = "Name: " + self.name + " Parents: " + ", ".join([p.name for p in self.parents]) + " Children: " + ", ".join([k.name for k in self.kids]) + "\n"
+        for instr in self.instrs:
+            s += "\t" + str(instr) + "\n"
+        return s
 
     def __lt__(self, other):
         self.num < other.num
 
     def add_prnt(self, parent):
         self.parents.append(parent)
+
+    def add_kid(self, kid):
+        self.kids.append(kid)
 
 def make_bb(function):
     num = 0
@@ -28,8 +37,10 @@ def make_bb(function):
     for instr in function["instrs"]:
         if "op" in instr:
             curr_instrs.append(instr)
-            if instr in bb.TERM:
+            if instr["op"] in bb.TERM:
                 blocks[curr_name] = bb(curr_instrs, curr_name, num, function["name"])
+                if instr["op"] == "jmp" or instr["op"] == "br":
+                    blocks[curr_name].kids += instr["labels"]
                 curr_instrs = []
                 curr_name = "temp" + str(len(blocks))
                 num += 1
@@ -37,35 +48,28 @@ def make_bb(function):
             if curr_instrs:
                 blocks[curr_name] = bb(curr_instrs, curr_name, num, function["name"])
                 num += 1
-
             curr_instrs = [instr]
             curr_name = instr["label"]
-
     if curr_instrs:
         blocks[curr_name] = bb(curr_instrs, curr_name, num, function["name"])
         num += 1
 
-    return blocks
-
-def make_cfg(blocks):
+    # make cfg
     for name in blocks:
-        block = blocks[name]
-        if not (block.term["op"] in bb.TERM):
-            continue
+        parents = blocks[name].kids
+        for i in range(len(blocks[name].kids)):
+            parent = blocks[name].kids[i]
+            blocks[name].kids[i] = blocks[parent]
+            blocks[parent].add_prnt(blocks[name])
 
-        labels = block.term["labels"]
-        for label in labels:
-            if label not in blocks:
-                raise Exception("Somehow have a label that is not associated with a block")
-            child = blocks[label]
-            child.add_prnt(blocks[label])
+    return blocks
 
 def reconstruct_prog(blocks):
     ofmap = {} # ordered functions map
     functions = []
     for name in blocks:
         block = blocks[name]
-        if block.func_name in ofmap.keys():
+        if block.fuc_name in ofmap.keys():
             ofmap[block.func_name].append(block)
         else:
             ofmap[block.func_name] = [block]
@@ -80,14 +84,65 @@ def reconstruct_prog(blocks):
 
     return functions
 
+def perform_op(instr):
+    if instr["op"] == "add":
+        return instr["args"][0] + instr["args"][1]
+    elif instr["op"] == "mul":
+        return instr["args"][0] * instr["args"][1]
+    elif instr["op"] == "sub":
+        return instr["args"][0] - instr["args"][1]
+    elif instr["op"] == "div":
+        return instr["args"][0] / instr["args"][1]
+    else:
+        raise Exception(f"Does not support instruction: {instr}")
+
+def constprop(block, ct = {}):
+    change = True
+    while(change):
+        change = False
+        for idx in range(len(block.instrs)):
+            instr = block.instrs[idx]
+            if "op" not in instr:
+                continue
+            if instr["op"] == "const" and instr["dest"] not in ct:
+                ct[instr["dest"]] = instr["value"]
+                change = True
+            if "args" not in instr:
+                continue
+            const = True
+            for arg in instr["args"]:
+                if arg in ct:
+                    instr["args"][instr["args"].index(arg)] = ct[arg]
+                if type(arg) != int:
+                    const = False
+            if const:
+                value = perform_op(instr)
+                block.instrs[idx] = {'dest': instr["dest"], 'op': 'const', 'type': 'int', 'value': value}
+                ct[instr["dest"]] = value
+                change = True
+    return block, ct
+
 def opt(prog):
     blocks = {}
     for func in prog["functions"]:
         blocks |= make_bb(func)
-    make_cfg(blocks)
-    for name in blocks:
-        block = blocks[name]
-        print(block)
+
+    stack = [blocks[name] for name in blocks]
+    while stack:
+        block = stack.pop()
+        if len(block.parent_state) != len(block.parents):
+            stack.append(block)
+            continue
+        ct = {}
+        for state in block.parent_state:
+            ct |= state
+        block, ct = 
+
+    #for name in blocks:
+    #    block = blocks[name]
+    #    print(block)
+    #    block, ct = constprop(block)
+    #    print(block, ct)
 
     return prog
 
