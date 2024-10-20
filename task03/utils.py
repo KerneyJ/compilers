d5196fc1d848f3096d4fc09b23793100f135d23c
kerneyJ <gamerguy9801@gmail.com>
2024-10-19 23:23:20 -0500

    added new util

--- task03/utils.py
+++ task03/utils.py
@@ -1,2 +1,7 @@
 def count(prog):
     return sum([len(f["instrs"]) for f in prog["functions"]])
+
+def print_instrs(prog):
+    for f in prog["functions"]:
+        for instr in f["instrs"]:
+            print(instr)
