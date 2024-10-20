f234ec4d61b5ef3cc18ce33eef48e25b9e371c56
kerneyJ <gamerguy9801@gmail.com>
2024-10-20 18:19:16 -0500

    print instrs for debuging

--- task03/utils.py
+++ task03/utils.py
@@ -5,3 +5,10 @@ def print_instrs(prog):
     for f in prog["functions"]:
         for instr in f["instrs"]:
             print(instr)
+
+if __name__ == "__main__":
+    import json
+    import sys
+    prog = json.load(sys.stdin)
+    # json.dump(prog, sys.stdout, indent=2)
+    print_instrs(prog)
