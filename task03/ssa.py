4e9316e8defc05c48e2a9db12ef8bdf7e06225a7
kerneyJ <gamerguy9801@gmail.com>
2024-10-19 19:07:01 -0500

    added find_phi, and adding typing to defs in get_defs

--- task03/ssa.py
+++ task03/ssa.py
@@ -12,7 +12,22 @@ def insert_phi(dest: str, typ: str, labels: list[str], args: list[str], block: c
              "args": args}
     bb.instrs.insert(0, instr)
 
-def get_defs(blocks):
+def modify_phi(block: cfg.bb):
+    pass
+
+def find_phi(var: str, block: cfg.bb):
+    for idx in range(len(block.instrs)):
+        instr = block.instrs[idx]
+        if "op" not in instr:
+            continue
+        if instr["op"] != "phi":
+            continue
+        if instr["dest"] == var:
+            return idx
+    return -1
+
+
+def get_defs(blocks: dict[str, cfg.bb]):
     map = {}
     for name in blocks:
         block = blocks[name]
@@ -20,10 +35,11 @@ def get_defs(blocks):
             if "dest" not in instr:
                 continue
             dest = instr["dest"]
+            typ = instr["type"]
             if dest in map:
-                map[dest].add(block)
+                map[dest][1].add(block)
             else:
-                map[dest] = set([block])
+                map[dest] = (typ, set([block]))
 
     return map
 
@@ -41,10 +57,11 @@ def to_ssa(blocks):
     defs = get_defs(blocks)
     for var in defs:
         print(var, end=": ")
-        for def_block in defs[var]:
+        for def_block in defs[var][1]:
             print(def_block.name, end=" => ")
             for df_block in dfs[def_block.name]:
-                print(df_block.name)
+                print(df_block.name, end=", ")
+            print()
         print()
 
 def opt(prog):
