3d5ced8159db89c71e67ce013488dfad23b41bc8
kerneyJ <gamerguy9801@gmail.com>
2024-10-21 04:40:19 -0500

    ssa bug fix

--- task03/ssa.py
+++ task03/ssa.py
@@ -1,4 +1,5 @@
 import json
+import copy
 import sys
 import cfg
 import gdce
@@ -34,10 +35,10 @@ def find_phi(var: str, block: cfg.bb):
             return idx
     return -1
 
-def find_var(var: str, vs: dict[str: (int, list[str])]):
+def find_var(var: str, vs: dict[str: list[str]]):
     potentials = [base for base in vs if base in var]
     for p in potentials:
-        stack = vs[p][1]
+        stack = vs[p]
         for i in range(len(stack)):
             if stack[i] == var:
                 return (p, i)
@@ -61,28 +62,32 @@ def get_defs(blocks: dict[str, cfg.bb]):
 
 def rename(entry: cfg.bb, params: dict[str, dict[str, str]], blocks: dict[str, cfg.bb]):
 
-    def fresh_name(var: str, vs: dict[str: (int, list[str])]):
-        if var not in vs:
-            vs[var] = [1, [var + ".1"]]
-            return vs[var][1][0]
-
-        vs[var][0] += 1
-        nn = var + "." + str(vs[var][0])
-        vs[var][1].insert(0, nn)
+    var_count = {}
+    def fresh_name(var: str, vs: dict[str: list[str]]):
+        if var not in var_count:
+            var_count[var] = 1
+        else:
+            var_count[var] += 1
+        nn = var + "." + str(var_count[var])
+        if var in vs:
+            vs[var].insert(0, nn)
+        else:
+            vs[var] = [nn]
         return nn # new_name
 
     var_stack = {}
     for param in params:
         var_name = param["name"]
-        var_stack[var_name] = (0, [var_name])
+        var_stack[var_name] = [var_name]
 
     def rename_helper(block, var_stack):
         label = block.name.split("@")[0]
+        my_var_stack = copy.deepcopy(var_stack)
 
         for phi_idx in block.gather_phi():
             instr = block.instrs[phi_idx]
             dest = instr["dest"]
-            new_name = fresh_name(dest, var_stack)
+            new_name = fresh_name(dest, my_var_stack)
             instr["dest"] = new_name
             block.instrs[phi_idx] = instr
 
@@ -92,11 +97,11 @@ def rename(entry: cfg.bb, params: dict[str, dict[str, str]], blocks: dict[str, c
                 continue
 
             if "args" in instr:
-                new_args = [var_stack[arg][1][0] for arg in instr["args"]]
+                new_args = [my_var_stack[arg][0] for arg in instr["args"]]
                 instr["args"] = new_args
 
             if "dest" in instr:
-                new_name = fresh_name(instr["dest"], var_stack)
+                new_name = fresh_name(instr["dest"], my_var_stack)
                 instr["dest"] = new_name
 
             block.instrs[idx] = instr
@@ -105,7 +110,7 @@ def rename(entry: cfg.bb, params: dict[str, dict[str, str]], blocks: dict[str, c
             kid_phis = kid.gather_phi()
             for phi_idx in kid_phis:
                 phi = kid.instrs[phi_idx]
-                dest = phi["dest"] if phi["dest"] in var_stack else find_var(phi["dest"], var_stack)
+                dest = phi["dest"] if phi["dest"] in my_var_stack else find_var(phi["dest"], my_var_stack)
                 if dest:
                     stack_pos = 0 # init to top of stack
                     if isinstance(dest, tuple):
@@ -113,12 +118,11 @@ def rename(entry: cfg.bb, params: dict[str, dict[str, str]], blocks: dict[str, c
                         dest = dest[0]
                     args = phi["args"]
                     arg_idx = phi["labels"].index(label)
-                    args[arg_idx] = var_stack[dest][1][stack_pos]
+                    args[arg_idx] = my_var_stack[dest][stack_pos]
                     phi["args"] = args
                     kid.instrs[phi_idx] = phi
                 else:
                     args = phi["args"]
-                    print(label, phi["labels"], phi)
                     arg_idx = phi["labels"].index(label)
                     args[arg_idx] = "__undefined"
                     phi["args"] = args
@@ -126,7 +130,7 @@ def rename(entry: cfg.bb, params: dict[str, dict[str, str]], blocks: dict[str, c
 
         for kid in block.kids:
             if kid in block.dominates:
-                rename_helper(kid, var_stack)
+                rename_helper(kid, my_var_stack)
 
     rename_helper(entry, var_stack)
 
