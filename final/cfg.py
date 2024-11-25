d31a132be8cc71c3458c27cb4dd51faa3b86d4b1
kerneyJ <gamerguy9801@gmail.com>
2024-11-25 17:02:35 -0600

    changes to final

--- final/cfg.py
+++ final/cfg.py
@@ -1,38 +1,22 @@
 import copy
 
 class bb:
-    TERM = ["br", "jmp", "ret", "call"]
-    COUNTER = 0
+    TERM = ['br', 'jmp', 'ret']
 
     def __init__(self, instrs, name, num, func_name):
         self.instrs = instrs
-        self.func_name = func_name
         self.name = name
-        self.num = num
-        self.term = self.instrs[-1]
-
         self.parents = []
         self.kids = []
-        self.call_parents = []
-        self.call_kids = []
-        self.ret_parents = []
-        self.ret_kids = []
-
-        self.reachable = []
-
         self.const_table = {}
-
         self.live_list = []
-
+        self.term = self.instrs[-1]
+        self.num = num
+        self.func_name = func_name
         self.dominates = []
 
-        self.var_to_mem = {} # maping of variables to allocations
-        self.live_alloc = set() # list of allocaitons made live by a load
-
     def __str__(self):
-        s = "Name: " + self.name + " Parents: {" + ", ".join([p.name for p in self.parents]) + "} Children: {" + ", ".join([k.name for k in self.kids]) +\
-            "} Call Parents: {" + ", ".join([p.name for p in self.call_parents]) + "} Call Children: {" + ", ".join([k.name for k in self.call_kids]) +\
-             "} Return Parents: {" + ", ".join([p.name for p in self.call_parents]) + "} Return Children: {" + ", ".join([k.name for k in self.call_kids]) + "}\n"
+        s = "Name: " + self.name + " Parents: " + ", ".join([p.name for p in self.parents]) + " Children: " + ", ".join([k.name for k in self.kids]) + "\n"
         for instr in self.instrs:
             s += "\t" + str(instr) + "\n"
         return s
@@ -102,121 +86,37 @@ def make_bb(function):
     blocks = {}
     curr_instrs = []
     curr_name = "entry@" + function["name"]
-    caller_name = None # switch that needs to be checked to see if the previous block ends with a call instruction, thus the this new block will be the block returned to
     for instr in function["instrs"]:
         if "op" in instr:
             curr_instrs.append(instr)
-            if instr["op"] not in bb.TERM:
-                continue
-            if caller_name:
-                blocks[caller_name].kids = [curr_name]
-                caller_name = None # consume caller name
-            if "call" in instr["op"]:
-                funcs = "_".join([func for func in instr["funcs"]])
-                caller_name = curr_name
+            if instr["op"] in bb.TERM:
                 blocks[curr_name] = bb(curr_instrs, curr_name, num, function["name"])
+                if instr["op"] == "jmp" or instr["op"] == "br":
+                    blocks[curr_name].kids += [label + "@" + function["name"] for label in instr["labels"]]
                 curr_instrs = []
-                curr_name = "ret-" + funcs + "@" + caller_name
+                curr_name = "temp" + str(len(blocks)) + "@" + function["name"]
                 num += 1
-                continue
-            blocks[curr_name] = bb(curr_instrs, curr_name, num, function["name"])
-            if instr["op"] == "jmp" or instr["op"] == "br":
-                blocks[curr_name].kids += [label + "@" + function["name"] for label in instr["labels"]]
-            curr_instrs = []
-            curr_name = "temp" + str(len(blocks)) + "@" + function["name"]
-            num += 1
         else: # we have a label
             if curr_instrs:
                 blocks[curr_name] = bb(curr_instrs, curr_name, num, function["name"])
-                if caller_name:
-                    blocks[caller_name].kids = [curr_name]
-                    caller_name = None # consume caller name
                 blocks[curr_name].kids = [instr["label"] + "@" + function["name"]]
                 num += 1
-
             curr_instrs = [instr]
             curr_name = instr["label"] + "@" + function["name"]
-            if caller_name:
-                blocks[caller_name].kids = [curr_name]
-                caller_name = None # consume caller name
     if curr_instrs:
         blocks[curr_name] = bb(curr_instrs, curr_name, num, function["name"])
         num += 1
-    if caller_name:
-        if curr_name in blocks:
-            blocks[caller_name].kids = [curr_name]
-            caller_name = None
-        else:
-            curr_instrs = [{"op": "ret"}]
-            blocks[curr_name] = bb(curr_instrs, curr_name, num, function["name"])
-            blocks[caller_name].kids = [curr_name]
 
 
     # make cfg
     for name in blocks:
-        block = blocks[name]
-        for idx in range(len(block.kids)):
-            kid_name = block.kids[idx]
-            kid_block = blocks[kid_name]
-            block.kids[idx] = kid_block
-            kid_block.parents.append(block)
+        parents = blocks[name].kids
+        for i in range(len(parents)):
+            parent = blocks[name].kids[i]
+            blocks[name].kids[i] = blocks[parent]
+            blocks[parent].add_prnt(blocks[name])
 
     return blocks
-
-def make_crg(blocks: dict[str, bb]): # crg => call ret graph
-    # first handle all of the calls
-    for name in blocks:
-        block = blocks[name]
-        term = block.term
-        if "op" not in term:
-            continue
-        if "call" not in term["op"]:
-            continue
-        funcs = term["funcs"]
-        for func in funcs:
-            block.call_kids.append(blocks["entry@" + func])
-            blocks["entry@" + func].call_parents.append(block)
-
-    # next handle all explicit returns
-    for name in blocks:
-        block = blocks[name]
-        term = block.term
-        if "op" not in term:
-            continue
-        if "ret" not in term["op"]:
-            continue
-        func_name = block.func_name
-        func_entry = blocks["entry@" + func_name]
-        callers = func_entry.call_parents
-        for caller in callers:
-            assert len(caller.kids) == 1 # the callers only child should be the return block
-            kid = caller.kids[0]
-            block.ret_kids.append(kid)
-            kid.ret_parents.append(block)
-
-    # finally, implicit returns
-    flb = {} # maps function names to last block of function
-    for name in blocks:
-        block = blocks[name]
-        if block.func_name not in flb:
-            flb[block.func_name] = block
-        else:
-            curr = flb[block.func_name]
-            if curr.num < block.num:
-                flb[block.func_name] = block
-
-    for name in flb:
-        block = flb[name]
-        term = block.term
-        if "ret" in term["op"]:
-            continue
-        entry = blocks["entry@" + name]
-        callers = entry.call_parents
-        for caller in callers:
-            assert len(caller.kids) == 1
-            kid = caller.kids[0]
-            block.ret_kids.append(kid)
-            kid.ret_parents.append(block)
 
 def insert_block(block: bb, child: bb, blocks):
     block.parents = copy.deepcopy(child.parents)
@@ -289,44 +189,3 @@ def df_b(b: bb, blocks: dict[str, bb]):
         if (block not in b.dominates) and len(set(block.parents) & set(b.dominates)) > 0:
             df.append(block)
     return df
-
-def reachable(parent: bb, child: bb):
-    stack = [parent]
-    visited = []
-    visited_funcs = set()
-    while stack:
-        block = stack.pop(0)
-        visited_funcs.add(block.func_name)
-        visited.append(block)
-        if child in (block.kids + block.ret_kids or block.call_kids):
-            continue
-        for kid in block.kids + block.call_kids:
-            if kid in visited:
-                continue
-            stack.append(kid)
-        for kid in block.ret_kids:
-            if kid in visited:
-                continue
-            if kid.func_name not in visited_funcs:
-                continue
-            stack.append(kid)
-    return visited
-
-if __name__ == "__main__":
-    import json
-    import sys
-    prog = json.load(sys.stdin)
-
-    blocks = {}
-    args = {}
-    for func in prog["functions"]:
-        blocks |= make_bb(func)
-        if "args" in func:
-            args[func["name"]] = func["args"]
-        else:
-            args[func["name"]] = []
-
-    make_crg(blocks)
-    reconstruct_prog(blocks, prog)
-
-    json.dump(prog, sys.stdout, indent=2)
